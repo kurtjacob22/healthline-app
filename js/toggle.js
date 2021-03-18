486ab098c3ea59dce43404d531a5553164749abf
kurtjacob <kurtjacob.urquico@gmail.comb>
2021-03-18 11:54:25 +0800

    fix sending insufficient data

--- js/toggle.js
+++ js/toggle.js
@@ -8,28 +8,33 @@ let msg = document.querySelector("#message");
 chkBoxToggle.addEventListener("change", () => {
   if (chkBoxToggle.checked) {
     anonText.style.color = "#d5a106";
-    em.style.display = "none";
+    // em.style.display = "none";
+    em.disabled = true;
     em.value = "";
   } else {
     anonText.style.color = "white";
     em.style.display = "";
+    em.disabled = false;
   }
 });
 
 // EMAIL
 
 const sendEmail = () => {
-  let userInfo = {
-    from_name: em.value + " codename: " + cdname.value,
-    course_block: crs.value,
-    message: msg.value,
-  };
-
-  emailjs.send("service_mf97w4n", "template_n003jf8", userInfo).then((res) => {
-    alert("message sent", res.status);
-    em.value = "";
-    cdname.value = "";
-    crs.value = "";
-    msg.value = "";
-  });
+  if (cdname.value != "" && crs.value != "" && msg.value != "") {
+    let userInfo = {
+      from_name: em.value + " codename: " + cdname.value,
+      course_block: crs.value,
+      message: msg.value,
+    };
+    // alert("Please Wait...");
+    emailjs
+      .send("service_mf97w4n", "template_n003jf8", userInfo)
+      .then((res) => {
+        alert("message sent", res.status);
+        window.location.href = "../../index.html";
+      });
+  } else {
+    alert("Please enter something");
+  }
 };
